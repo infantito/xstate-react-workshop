d8e55c2b450ee893abdea91985cc30f797e3dd06
infantito <danielinfantito61@gmail.com>
2021-01-26 22:58:30 -0500

    perf: refactoring with useRefucer

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -2,13 +2,42 @@ import * as React from 'react'
 
 const TIMEOUT = 2000
 
+const INITIAL_STATE = 'inactive'
+
+const alarmReducer = (state, event) => {
+  switch (state) {
+    case 'inactive':
+      if (event.type === 'TOGGLE') {
+        return 'pending'
+      }
+
+      return state
+    case 'pending':
+      if (event.type === 'SUCCESS') {
+        return 'active'
+      } else if (event.type === 'TOGGLE') {
+        return 'inactive'
+      }
+
+      return state
+    case 'active':
+      if (event.type === 'TOGGLE') {
+        return 'inactive'
+      }
+
+      return state
+    default:
+      return 'inactive'
+  }
+}
+
 export const ScratchApp = () => {
-  const [status, setStatus] = React.useState('inactive')
+  const [status, dispatch] = React.useReducer(alarmReducer, INITIAL_STATE)
 
   React.useEffect(() => {
     if (status === 'pending') {
       const timer = setTimeout(() => {
-        setStatus('active')
+        dispatch({type: 'SUCCESS'})
       }, TIMEOUT)
 
       return () => clearTimeout(timer)
@@ -28,11 +57,7 @@ export const ScratchApp = () => {
           className="alarmToggle"
           style={{opacity: status === 'pending' ? 0.5 : 1}}
           data-active={status === 'active' || void 0}
-          onClick={() =>
-            setStatus(prevStatus =>
-              prevStatus === 'active' ? 'inactive' : 'pending',
-            )
-          }
+          onClick={() => dispatch({type: 'TOGGLE'})}
         ></div>
       </div>
     </div>
